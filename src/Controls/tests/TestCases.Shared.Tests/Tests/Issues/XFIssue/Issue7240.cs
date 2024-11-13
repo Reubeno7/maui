fd2a4cfc713ba0b341043e6f980933763440f82d
Reubeno7 <reubeno@syncfusion.com>
2024-11-13 08:03:51 +0300

    Handled Bugzilla issue 7240 and 3019 issue 7240 - added a dynamic automationId to the child elements. issue 3019 - restructured the code and introduced new instances which were initialized implicitly using the keyword var, though I changed the structure of the UI sample code and the test case code, I believe that the logic is still the same.

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/XFIssue/Issue7240.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/XFIssue/Issue7240.cs
@@ -2,24 +2,27 @@
 using UITest.Appium;
 using UITest.Core;
 
-namespace Microsoft.Maui.TestCases.Tests.Issues;
-
-public class Issue7240 : _IssuesUITest
+namespace Microsoft.Maui.TestCases.Tests.Issues
 {
-	public Issue7240(TestDevice testDevice) : base(testDevice)
+	public class Issue7240 : _IssuesUITest
 	{
+		private string ClickMe = "ClickMe";
+
+		public Issue7240(TestDevice testDevice) : base(testDevice)
+		{
+		}
+
+		public override string Issue => "[Android] Shell content layout hides navigated to page";
+
+		[Test]
+		[Category(UITestCategories.Shell)]
+		public void ShellSecondPageHasSameLayoutAsPrimary()
+		{
+			App.WaitForElement(ClickMe);
+			App.Tap(ClickMe);
+			App.WaitForElement("Page Count:2");
+			App.Tap(ClickMe);
+			App.WaitForElement("Success");
+		}
 	}
-
-	public override string Issue => "[Android] Shell content layout hides navigated to page";
-
-	//[Test]
-	//[Category(UITestCategories.Shell)]
-	//public void ShellSecondPageHasSameLayoutAsPrimary()
-	//{
-	//	App.WaitForElement(ClickMe);
-	//	App.Tap(ClickMe);
-	//	App.WaitForElement("Page Count:2");
-	//	App.Tap(ClickMe);
-	//	App.WaitForElement(Success);
-	//}
 }
