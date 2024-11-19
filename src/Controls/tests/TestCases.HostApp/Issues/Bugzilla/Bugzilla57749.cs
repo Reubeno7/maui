0f2f722a5e28dcedf3669d35680774b8fd0ce004
Reubeno7 <reubeno@syncfusion.com>
2024-11-19 17:07:56 +0300

    57749 - ensured that the elements were properly and implicitly initialized, added the automationId's and were correctly added into the  layout, as a result the automation was able to run smoothly and it was a success.

--- src/Controls/tests/TestCases.HostApp/Issues/Bugzilla/Bugzilla57749.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Bugzilla/Bugzilla57749.cs
@@ -1,27 +1,71 @@
-namespace Maui.Controls.Sample.Issues;
-
-[Issue(IssueTracker.Bugzilla, 57749, "After enabling a disabled button it is not clickable", PlatformAffected.UWP)]
-public class Bugzilla57749 : TestContentPage
+namespace Maui.Controls.Sample.Issues
 {
-	protected override void Init()
+	[Issue(IssueTracker.Bugzilla, 57749, "After enabling a disabled button it is not clickable", PlatformAffected.UWP)]
+	public class Bugzilla57749 : TestContentPage
 	{
-		button1.Text = "Click me";
-		button1.AutomationId = "btnClick";
-		button1.IsEnabled = false;
-		button1.Clicked += Button1_Clicked1;
-		this.Content = button1;
-	}
-	Button button1 = new Button();
+		protected override void Init()
+		{
+			button1.Text = "Click me";
+			button1.AutomationId = "btnClick";
+			button1.IsEnabled = false;
+			button1.Clicked += Button1_Clicked1;
+			this.Content = button1;
+		}
+		Button button1 = new Button();
 
-	private void Button1_Clicked1(object sender, EventArgs e)
-	{
-		this.DisplayAlert("Button test", "Button was clicked", "Ok");
+		private async void Button1_Clicked1(object sender, EventArgs e)
+		{
+			var customAlert = new CustomAlertPage();
+			await Navigation.PushModalAsync(customAlert);
+		}
+
+		protected override async void OnAppearing()
+		{
+			base.OnAppearing();
+			await Task.Delay(100);
+			button1.IsEnabled = true;
+		}
 	}
 
-	protected override async void OnAppearing()
+	public class CustomAlertPage : ContentPage
 	{
-		base.OnAppearing();
-		await Task.Delay(100);
-		button1.IsEnabled = true;
+		public CustomAlertPage()
+		{
+			var stackLayout = new StackLayout
+			{
+				Padding = new Thickness(20),
+				VerticalOptions = LayoutOptions.Center,
+				HorizontalOptions = LayoutOptions.Center
+			};
+
+			var titleLabel = new Label
+			{
+				Text = "Button test",
+				FontSize = 20,
+				HorizontalOptions = LayoutOptions.Center
+			};
+
+			var messageLabel = new Label
+			{
+				Text = "Button was clicked",
+				FontSize = 16,
+				HorizontalOptions = LayoutOptions.Center,
+				AutomationId = "Button was clicked"
+			};
+
+			var okButton = new Button
+			{
+				Text = "Ok",
+				HorizontalOptions = LayoutOptions.Center,
+				AutomationId = "Ok"
+			};
+			okButton.Clicked += async (s, e) => await Navigation.PopModalAsync();
+
+			stackLayout.Children.Add(titleLabel);
+			stackLayout.Children.Add(messageLabel);
+			stackLayout.Children.Add(okButton);
+
+			Content = stackLayout;
+		}
 	}
 }
